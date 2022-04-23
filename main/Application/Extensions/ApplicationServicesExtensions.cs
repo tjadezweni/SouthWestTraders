6046849d8f357ce8cb6d7d93572339c37cf05f59
tjadezweni <tjadezweni@jhb.dvt.co.za>
2022-04-24 00:23:04 +0200

    setup: created service registration class for application services registration

---
+++ main/Application/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Application.Infrastructure;
+using Application.Infrastructure.Repositories.Orders;
+using Application.Infrastructure.Repositories.OrderStates;
+using Application.Infrastructure.Repositories.Products;
+using Application.Infrastructure.Repositories.Stocks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static void RegisterDbContext(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<SouthWestTradersDBContext>(options =>
+            {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DbConnection"));
+            });
+        }
+
+        public static void RegisterRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IStockRepository, StockRepository>();
+            services.AddScoped<IOrderStateRepository, OrderStateRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+        }
+    }
+}
