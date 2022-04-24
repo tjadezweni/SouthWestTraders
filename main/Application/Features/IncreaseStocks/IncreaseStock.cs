3e3eb714dd13f8ec085ac6dad2555518b95e282a
tjadezweni <tjadezweni@jhb.dvt.co.za>
2022-04-25 00:24:38 +0200

    feat: implemented increase product stock feature

---
+++ main/Application/Features/IncreaseStocks/IncreaseStock.cs
@@ -0,0 +1,38 @@
+﻿using Application.Exceptions;
+using Application.Infrastructure.Repositories.Stocks;
+using MediatR;
+
+namespace Application.Features.IncreaseStocks
+{
+    public static class IncreaseStock
+    {
+        public record Command : IRequest<Unit>
+        {
+            public int ProductId { get; set; }
+            public int StockAmount { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly IStockRepository _stockRepository;
+
+            public Handler(IStockRepository stockRepository)
+            {
+                _stockRepository = stockRepository;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var stock = await _stockRepository.GetStockWithProductId(request.ProductId);
+                if (stock is null)
+                {
+                    throw new ProductNotFoundException(request.ProductId);
+                }
+                int newStockAmount = stock.AvailableStock + request.StockAmount;
+                stock.AvailableStock = newStockAmount;
+                await _stockRepository.UpdateAsync(stock);
+                return Unit.Value;
+            }
+        }
+    }
+}
