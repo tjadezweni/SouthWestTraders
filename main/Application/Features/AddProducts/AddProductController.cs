e8c851b6a4d14fbf13124c79964285d621f50ba3
tjadezweni <tjadezweni@jhb.dvt.co.za>
2022-04-24 21:32:49 +0200

    feat: now i finished remove product feature implementation

--- main/Application/Features/AddProducts/AddProductController.cs
+++ main/Application/Features/AddProducts/AddProductController.cs
@@ -1,8 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace Application.Features.AddProducts
 {
     [Route("/api/products")]
@@ -16,7 +14,7 @@ namespace Application.Features.AddProducts
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] AddProduct.Command command)
+        public async Task<IActionResult> AddProduct([FromBody] AddProduct.Command command)
         {
             var product = await _mediator.Send(command);
             return Created("", product);
