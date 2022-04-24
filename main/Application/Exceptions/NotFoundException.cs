58fa6990c8bdb53b984c29350f8819d423cdbd80
tjadezweni <tjadezweni@jhb.dvt.co.za>
2022-04-24 21:28:23 +0200

    feat: implemented delete product feature

---
+++ main/Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        protected NotFoundException(string entityType, int entityId)
+            : base(DefaultMessage(entityType, entityId))
+        {
+
+        }
+
+        private static string DefaultMessage(string entityType, int entityId)
+        {
+            return $"The {entityType} with id: {entityId} was not found.";
+        }
+    }
+}
