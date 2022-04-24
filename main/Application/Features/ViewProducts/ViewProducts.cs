d3e0eabf652fc57344f3f786116882578d1f6189
tjadezweni <tjadezweni@jhb.dvt.co.za>
2022-04-24 23:38:04 +0200

    feat: implemented view products feature

---
+++ main/Application/Features/ViewProducts/ViewProducts.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.AddProducts;
+using Application.Infrastructure.Repositories.Products;
+using MediatR;
+
+namespace Application.Features.ViewProducts
+{
+    public static class ViewProducts
+    {
+        public record Query : IRequest<IEnumerable<ProductDto>>
+        {
+
+        }
+
+        public class Handler : IRequestHandler<Query, IEnumerable<ProductDto>>
+        {
+            private readonly IProductRepository _productRepository;
+
+            public Handler(IProductRepository productRepository)
+            {
+                _productRepository = productRepository;
+            }
+
+            public async Task<IEnumerable<ProductDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var productsList = await _productRepository.ListAsync(product => true);
+                var productsDtoList = productsList.Select(product => new ProductDto { ProductId = product.ProductId, Name = product.Name, 
+                    Description = product.Description, Price = product.Price });
+                return productsDtoList;
+            }
+        }
+    }
+}
